4f3df9912508bf5c4f61d8091ea28444b7d5b353
weibo <919006380@qq.com>
2019-06-29 07:44:36 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -1,45 +1,32 @@
-```
 echo 复制deploy_plugin下的所有目录到dist/js
-
 cp -r deploy_plugin/* dist/js/
 
 echo 压缩部署包！
-
 tar -zcvf dist.tar.gz dist/css/ dist/js/ dist/img/ dist/index.html
 
 echo 上传文件
-
 scp -r dist.tar.gz user@remote:/www/admin
 
-\# 服务器环境开启
-
+# 服务器环境开启
 ssh user@remote -tt <<EOF
 
 echo 进入目标目录
-
 cd www/admin
 
 echo 删除css/ js/ img/ index.html
-
 rm -rf css/ js/ img/ index.html
 
 echo 解压dist.tar.gz
-
 tar -zxvf dist.tar.gz --strip-components 1
 
 echo 删除本地压缩包！
-
 rm -rf dist.tar.gz
 
 exit
-
 EOF
 
-\# 服务器环境结束
-
+# 服务器环境结束
 echo 上传完成！
 
 echo 删除本地压缩包！
-
 rm -rf dist.tar.gz
-```
